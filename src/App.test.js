c7f7ce99ab10993351e2db52b5119d378e56a76b
douglasdotbyers <douglasdotbyers@gmail.com>
2017-09-17 22:44:33 +0100

    Remove example code.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('should create the app', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App message="there" />, div);
 });
